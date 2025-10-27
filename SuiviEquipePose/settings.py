afe98840def90012b9b3189bcb6c46461e5051d1
billel.benmakhlouf <billel.benmakhlouf@gmail.com>
2025-10-27 14:05:14 +0100

    ok

--- SuiviEquipePose/settings.py
+++ SuiviEquipePose/settings.py
@@ -10,14 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-"""import dj_database_url
-import os"""
+"""import dj_database_url """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
